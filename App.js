958148c847361c87e90cf8d926511302b69bbaa8
Galadriel Cho <42721783+galadrielcho@users.noreply.github.com>
2021-07-26 12:57:33 -0400

    set up camera in translation section
    container size needs to be fixed

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Button, TextInput, Image, Animated, TouchableHighlight, Alert } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Button, TextInput, Image, Animated, TouchableHighlight, Alert, TouchableOpacity } from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
-import React, {useState} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import AwesomeButton from "react-native-really-awesome-button";
 import * as Animatable from 'react-native-animatable';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
+import {drawRect} from "./utilities"; 
+import { Camera } from 'expo-camera';
 
 function StartScreen({navigation}) {
   return (
@@ -29,7 +31,7 @@ function StartScreen({navigation}) {
         medium
         secondary
         onPress={next => { navigation.navigate('TranslateDecision')
-          next();
+          next();   
         }}
       >
         Translation
@@ -74,22 +76,37 @@ function LearnDecisionScreen({navigation}) {
 }
 
 function TranslateDecisionScreen({navigation}) {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text>Translate stuff:</Text>
-      <AwesomeButton
-        backgroundColor="blue"
-        color = "#45bf65"
-        medium
-        secondary
-        onPress={next => { navigation.navigate('Home')
-          next();
-        }}
-      >
-        BACK
-    </AwesomeButton>
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
 
-    </SafeAreaView>
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+
+  }
+  return (
+    <View style={styles.container}>
+    <Camera style={styles.camera} type={type}>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={next => {navigation.navigate('Home')
+          next();
+        }}>
+          <Text style={styles.text}> Return </Text>
+        </TouchableOpacity>
+      </View>
+    </Camera>
+    </View>
   );
 }
 
@@ -118,6 +135,79 @@ const Stack = createStackNavigator();
 
 function App() {
 
+  const camRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  // Main function
+  const runCoco = async () => {
+    // 3. TODO - Load network 
+    // e.g. const net = await cocossd.load();
+    // https://tensorflowjsrealtimemodel.s3.au-syd.cloud-object-storage.appdomain.cloud/model.json
+    const net = await tf.loadGraphModel('https://tensorflowjsrealtimemodel.s3.au-syd.cloud-object-storage.appdomain.cloud/model.json')
+    
+    //  Loop and detect hands
+    setInterval(() => {
+      detect(net);
+    }, 16.7);
+  };
+
+  // takePicture = async () => {
+  //   if (this.camera) {
+  //     const options = { quality: 0.5, base64: true };
+  //     const data = await this.camera.takePictureAsync(options);
+  //     console.log(data.uri);
+  //   }
+  // };
+
+
+  // const detect = async (net) => {
+  //   // Check data is available
+  //   if (
+  //     typeof webcamRef.current !== "undefined" &&
+  //     webcamRef.current !== null &&
+  //     webcamRef.current.video.readyState === 4
+  //   ) {
+  //     // Get Video Properties
+  //     const video = webcamRef.current.video;
+  //     const videoWidth = webcamRef.current.video.videoWidth;
+  //     const videoHeight = webcamRef.current.video.videoHeight;
+
+  //     // Set video width
+  //     webcamRef.current.video.width = videoWidth;
+  //     webcamRef.current.video.height = videoHeight;
+
+  //     // Set canvas height and width
+  //     canvasRef.current.width = videoWidth;
+  //     canvasRef.current.height = videoHeight;
+
+  //     // 4. TODO - Make Detections
+  //     const img = tf.browser.fromPixels(video)
+  //     const resized = tf.image.resizeBilinear(img, [640,480])
+  //     const casted = resized.cast('int32')
+  //     const expanded = casted.expandDims(0)
+  //     const obj = await net.executeAsync(expanded)
+  //     console.log(obj)
+
+  //     const boxes = await obj[1].array()
+  //     const classes = await obj[2].array()
+  //     const scores = await obj[4].array()
+      
+  //     // Draw mesh
+  //     const ctx = canvasRef.current.getContext("2d");
+
+  //     // 5. TODO - Update drawing utility
+  //     // drawSomething(obj, ctx)  
+  //     requestAnimationFrame(()=>{drawRect(boxes[0], classes[0], scores[0], 0.8, videoWidth, videoHeight, ctx)}); 
+
+  //     tf.dispose(img)
+  //     tf.dispose(resized)
+  //     tf.dispose(casted)
+  //     tf.dispose(expanded)
+  //     tf.dispose(obj)
+
+  //   }
+  // };
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
@@ -138,6 +228,24 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFC0CB',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    margin: 20,
+  },
+  button: {
+    flex: 0.1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+    color: 'white',
   },
 
 });
